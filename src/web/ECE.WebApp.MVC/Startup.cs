224d9fc4462759e10797bf6c73c6140cf45a714b
Wesley-Silva <wesleybf3@gmail.com>
2023-06-22 21:05:08 -0300

    Aplicação WebMVC e configurando a autenticação

---
+++ src/web/ECE.WebApp.MVC/Startup.cs
@@ -0,0 +1,30 @@
+using ECE.WebApp.MVC.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECE.WebApp.MVC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityConfiguration();
+            services.AddMvcConfiguration();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            app.UseMvcConfiguration(env);
+        }
+    }
+}
