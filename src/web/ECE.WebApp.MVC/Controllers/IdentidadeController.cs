fbf354f314e1d1535aebacbdf43a9a114d964094
Wesley-Silva <wesleybf3@gmail.com>
2023-06-23 21:54:04 -0300

    Adiciona Models, Views e Controller de login

--- src/web/ECE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/ECE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,9 +1,69 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECE.WebApp.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace ECE.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
-        public IdentidadeController() { }
+        public IdentidadeController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(usuarioRegistro);
+            }
+
+            if (false)
+            {
+                return View(usuarioRegistro);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewData["returnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin, string returnUrl = null)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(usuarioLogin);
+            }
+
+            if (false)
+            {
+                return View(usuarioLogin);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public async Task<IActionResult> Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
