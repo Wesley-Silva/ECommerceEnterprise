58859a8b9a5e4eb174508deac7b6ea86744432d7
Wesley-Silva <wesleybf3@gmail.com>
2023-06-28 09:43:04 -0300

    Padronizando os objetos de retorno json

--- src/web/ECE.WebApp.MVC/Services/IAutenticationService.cs
+++ src/web/ECE.WebApp.MVC/Services/IAutenticationService.cs
@@ -5,7 +5,7 @@ namespace ECE.WebApp.MVC.Services
 {
     public interface IAutenticationService
     {
-        Task<string> Login(UsuarioLogin usuarioLogin);
-        Task<string> Registro(UsuarioRegistro usuarioRegistro);
+        Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin);
+        Task<UsuarioRespostaLogin> Registro(UsuarioRegistro usuarioRegistro);
     }
 }
