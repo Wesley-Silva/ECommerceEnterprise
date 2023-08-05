917ac7f95c5dee4d2dd65096fe1bb820e1eae1e6
Wesley-Silva <wesleybf3@gmail.com>
2023-08-05 20:25:30 -0300

    Tratamento de erros do servidor - pt I

---
+++ src/web/ECE.WebApp.MVC/Extensions/ExceptionMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ECE.WebApp.MVC.Extensions
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (CustomHttpRequestException ex)
+            {
+                HandlerRequestExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private void HandlerRequestExceptionAsync(HttpContext context, CustomHttpRequestException httpRequestException)
+        {
+            if (httpRequestException.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                context.Response.Redirect("/login");
+                return;
+            }
+
+            context.Response.StatusCode = (int)httpRequestException.StatusCode;
+        }
+    }
+}
