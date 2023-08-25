87341729e2baabcd4d5580e76d516dac01a59aa1
Wesley-Silva <wesleybf3@gmail.com>
2023-08-25 20:23:59 -0300

    Adiciona info do token dentro do request através do Delegating Handler

--- src/web/ECE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/ECE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,5 +1,6 @@
 ﻿using ECE.WebApp.MVC.Extensions;
 using ECE.WebApp.MVC.Services;
+using ECE.WebApp.MVC.Services.Handlers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,8 +10,12 @@ namespace ECE.WebApp.MVC.Configuration
     {
         public static void RegisterServices(this IServiceCollection services)
         {
+            services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
+
             services.AddHttpClient<IAutenticationService, AutenticationService>();
-            services.AddHttpClient<ICatalogoService, CatalogoService>();
+
+            services.AddHttpClient<ICatalogoService, CatalogoService>()
+                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUser, AspNetUser>();
---
+++ src/web/ECE.WebApp.MVC/Services/Handlers/HttpClientAuthorizationDelegatingHandler.cs
@@ -0,0 +1,39 @@
+﻿using ECE.WebApp.MVC.Extensions;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ECE.WebApp.MVC.Services.Handlers
+{
+    public class HttpClientAuthorizationDelegatingHandler : DelegatingHandler
+    {
+        private readonly IUser _user;
+
+        public HttpClientAuthorizationDelegatingHandler(IUser user)
+        {
+            _user = user;
+        }
+
+        // intercepita o request e coloca as info do token na autorização 
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var authorizationHandler = _user.ObterHttpContext().Request.Headers["Authorization"];
+
+            if (!string.IsNullOrEmpty(authorizationHandler))
+            {
+                request.Headers.Add("Authorization", new List<string>() { authorizationHandler });
+            }
+
+            var token = _user.ObterUserToken();
+
+            if (token != null)
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
