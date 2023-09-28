f297a6e756aa07555480dbfd4b820f14f3b9d372
Wesley-Silva <wesleybf3@gmail.com>
2023-09-27 21:36:49 -0300

    Adiciona objetos de valor e validações

---
+++ src/building blocks/ECE.Core/DomainObjects/DomainException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECE.Core.DomainObjects
+{
+    public class DomainException : Exception
+    {
+        public DomainException()
+        { }
+
+        public DomainException(string message) : base(message)
+        { }
+
+        public DomainException(string message, Exception innerException) : base(message, innerException)
+        { }
+    }
+}
