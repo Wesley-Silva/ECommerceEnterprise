78a5096d9feff864295e33c0a3cf019cf07bbf98
Wesley-Silva <wesleybf3@gmail.com>
2023-09-30 16:41:28 -0300

    Mapeamento de tabelas clientes e endereço

---
+++ src/services/ACE.Clientes.API/Configuration/DepencencyInjectionConfig.cs
@@ -0,0 +1,13 @@
+﻿using ACE.Clientes.API.Data;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ACE.Clientes.API.Configuration
+{
+    public static class DepencencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<ClientesContext>();
+        }
+    }
+}
---
+++ src/services/ACE.Clientes.API/Data/ClientesContext.cs
@@ -0,0 +1,44 @@
+﻿using ECE.Core.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using ACE.Clientes.API.Models;
+using System.Threading.Tasks;
+
+namespace ACE.Clientes.API.Data
+{
+    public class ClientesContext : DbContext, IUnitOfWork
+    {
+        public ClientesContext(DbContextOptions<ClientesContext> options)
+            : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                x => x.GetProperties().Where(p => p.ClrType == typeof(string))))
+            {
+                property.SetColumnType("varchar(100)");
+            }
+
+            // onde tem relacionamento desabilitar o delete em cascata
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(c => c.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            }
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClientesContext).Assembly);
+        }
+
+        public async Task<bool> Commit()
+        {
+            return await base.SaveChangesAsync() > 0;
+        }
+    }
+}
