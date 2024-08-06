c46313bae0b2aae6f59102337122198370ca6f07
Wesley-Silva <wesleybf3@gmail.com>
2024-08-06 17:32:42 -0300

    Validação de acesso baseado em claims

--- ECommerce/src/services/ECE.Catalogo.API/Configuration/SwaggerConfig.cs
+++ ECommerce/src/services/ECE.Catalogo.API/Configuration/SwaggerConfig.cs
@@ -18,6 +18,32 @@ namespace ECE.Catalogo.API.Configuration
                     Contact = new OpenApiContact() { Name = "Wesley Silva", Email = "teste@gmail.com" },
                     License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licences/MIT") }
                 });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                // Para metodos que requerem um acesso restrito
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
             });
         }
 
--- ECommerce/src/services/ECE.Catalogo.API/Controllers/CatalogoController.cs
+++ ECommerce/src/services/ECE.Catalogo.API/Controllers/CatalogoController.cs
@@ -1,5 +1,7 @@
 ﻿using ECE.Catalogo.API.Data.Repository;
 using ECE.Catalogo.API.Models;
+using ECE.WebAPI.Core.Identidade;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,6 +9,8 @@ using System.Threading.Tasks;
 
 namespace ECE.Catalogo.API.Controllers
 {
+    [ApiController]
+    [Authorize]
     public class CatalogoController : Controller
     {
         private readonly IProdutoRepository _produtoRepository;
@@ -16,12 +20,14 @@ namespace ECE.Catalogo.API.Controllers
             _produtoRepository = produtoRepository;
         }
 
+        [AllowAnonymous]
         [HttpGet("catalogo/produtos")]
         public async Task<IEnumerable<Produto>> Index()
         {
             return await _produtoRepository.ObterTodos();
         }
 
+        [ClaimsAuthorize("Catalogo", "Ler")]
         [HttpGet("catalogo/produtos/{id}")]
         public async Task<Produto> ProdutoDetalhe(Guid id)
         {
