2da22f82da5e3ed5f5f50c2cd7f31fab4d188a9a
Wesley-Silva <wesleybf3@gmail.com>
2024-11-08 18:27:00 -0300

    Adiciona configuração do Identity e documentação da API

--- src/services/ECE.Identidade.API/Startup.cs
+++ src/services/ECE.Identidade.API/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -39,11 +40,28 @@ namespace ECE.Identidade.API
                 .AddDefaultTokenProviders();
 
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "ECommerce Enterprise Identity API",
+                    Description = "API do curso ASP .NET Core Enterprise Application",
+                    Contact = new OpenApiContact() { Name = "Wesley Silva", Email = "teste@teste.com"},
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licences/MIT")}
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
