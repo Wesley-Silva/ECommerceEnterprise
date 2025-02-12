13736652ba08565466c9e92f9a4e080db303805b
Wesley-Silva <wesleybf3@gmail.com>
2025-02-12 18:21:54 -0300

    Adiciona boas práticas com HttpServices

--- src/web/ECE.WebApp.MVC/Services/AutenticacaoService.cs
+++ src/web/ECE.WebApp.MVC/Services/AutenticacaoService.cs
@@ -1,4 +1,6 @@
-﻿using ECE.WebApp.MVC.Models;
+﻿using ECE.WebApp.MVC.Extensions;
+using ECE.WebApp.MVC.Models;
+using Microsoft.Extensions.Options;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -7,17 +9,19 @@ namespace ECE.WebApp.MVC.Services
     public class AutenticacaoService : Service, IAutenticacaoService
     {
         private readonly HttpClient _httpClient;
+        private readonly AppSettings _settings;
 
-        public AutenticacaoService(HttpClient httpClient)
-        {
+        public AutenticacaoService(HttpClient httpClient, IOptions<AppSettings> settings)
+        {           
             _httpClient = httpClient;
+            _settings = settings.Value;
         }
 
         public async Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin)
         {
             var loginContent = ObterConteudo(usuarioLogin);
 
-            var response = await _httpClient.PostAsync("https://localhost:44329/api/identidade/autenticar", loginContent);
+            var response = await _httpClient.PostAsync($"{_settings.AutenticacaoUrl}/api/identidade/autenticar", loginContent);
 
             if (!TratarErrosResponse(response))
             {
@@ -27,8 +31,6 @@ namespace ECE.WebApp.MVC.Services
                 };
             }
 
-            //var teste = await response.Content.ReadAsStringAsync();
-
             return await DeserializarObjetoResponse<UsuarioRespostaLogin>(response);
         }
 
@@ -36,7 +38,7 @@ namespace ECE.WebApp.MVC.Services
         {
             var registroContent = ObterConteudo(usuarioRegistro);
 
-            var response = await _httpClient.PostAsync("https://localhost:44329/api/identidade/nova-conta", registroContent);
+            var response = await _httpClient.PostAsync($"{_settings.AutenticacaoUrl}/api/identidade/nova-conta", registroContent);
 
             if (!TratarErrosResponse(response))
             {
