c67d1add15485eab4c013370009f8ce725ad9cb0
dev <wesleybf3@gmail.com>
2025-12-02 18:55:32 -0300

    Ajuste para exibir icones no carrinho e logo

---
+++ src/web/ECE.WebApp.MVC/Extensions/CarrinhoViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using ECE.WebApp.MVC.Models;
+using ECE.WebApp.MVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ECE.WebApp.MVC.Extensions
+{
+    public class CarrinhoViewComponent : ViewComponent
+    {
+        private readonly ICarrinhoService _carrinhoService;
+
+        public CarrinhoViewComponent(ICarrinhoService carrinhoService)
+        {
+            _carrinhoService = carrinhoService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _carrinhoService.ObterCarrinho() ?? new CarrinhoViewModel());
+        }
+    }
+}
