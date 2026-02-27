6c57f4a1fa5de5845cc87c02259e8d2429f6af2a
dev <wesleybf3@gmail.com>
2026-02-27 17:40:54 -0300

    Implemeta query para obter voucher

--- src/services/ECE.Pedido.Domain/Vouchers/Voucher.cs
+++ src/services/ECE.Pedido.Domain/Vouchers/Voucher.cs
@@ -1,4 +1,5 @@
 ﻿using ECE.Core.DomainObjets;
+using ECE.Pedido.Domain.Vouchers.Specs;
 using System;
 
 namespace ECE.Pedido.Domain.Vouchers
@@ -15,5 +16,18 @@ namespace ECE.Pedido.Domain.Vouchers
         public DateTime DataValidade { get; private set; }
         public bool Ativo { get; private set; }
         public bool Utilizado { get; private set; }
+
+        public bool EstaValidoParaUtilizacao()
+        {
+            return new VoucherAtivoSpecification()
+                .And(new VoucherDataSpecification())
+                .And(new VoucherQuantidadeSpecification())
+                .IsSatisfiedBy(this);
+        }
+
+        public void MarcarComoUtilizado()
+        {
+
+        }
     }
 }
